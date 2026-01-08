d2408759c26290209c5ad615c6ed179de4fc59ab
nostalgia296 <3570965752@qq.com>
2026-01-08 23:20:09 +0800

    name

--- src/config.ts
+++ src/config.ts
@@ -58,7 +58,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
     avatar: "assets/images/ava.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    name: "LaiyuriAyaka",
+    name: "Laik",
     bio: "qwq",
     links: [{
         name: "GitHub",
